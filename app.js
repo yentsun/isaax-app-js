5ddc4f104f38e3ff46afdc1d7adcdbd35fab2dad
Kylych Tynybekov <ktynybekov@gmail.com>
2016-12-21 18:19:31 +0600

    modified

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ setInterval(function() {
   console.log("***********************");
   console.log("  New version message  ");
   console.log("***********************");
-  console.log(" end of message ")
-  console.log("Bye!")
   console.log("x_x")
   console.log("o_O")
   console.log("x_x")
+  console.log(" end of message ")
+  console.log("Bye!")
   console.log("End")
 }, 5000);
